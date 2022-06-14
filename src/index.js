6ca71d2af0d77d3d2d5f9d5d050a9a30948d9e0c
sid-al <sendplusx@gmail.com>
2022-06-14 16:10:20 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('form')
+  // log something
+   const newTaskForm = document.querySelector('#create-task-form');
+   const logTask = document.getElementById('new-task-description');
+   const nwTaskPriority = document.getElementById('new-task-priority');
+
+   // ul where new tasks will be in DOM
+
+   const nwTaskUl = document.getElementById('tasks');
+
+   // bind event listners
+
+   newTaskForm.addEventListener('submit', createNwTask); 
+  });
+
+
+const createNwTask = event => {
+  // stop from trying to submit
+  event.preventDefault();
+
+  const nwTaskDescription = document.getElementById('new-task-description');
+  const nwTask = document.createElement('li');
+  nwTask.innerHTML = nwTaskDescription.value;
+
+  appendnwTask(nwTask);
+  event.target.reset();
+};
+
+const appendnwTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+
